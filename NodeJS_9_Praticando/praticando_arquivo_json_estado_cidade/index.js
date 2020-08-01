3f02bb81e6932392f769d412d17dcfe5cdf8e769
cleber <cleber.email@gmail>
2020-08-01 08:48:29 -0300

    atualizando manipulação de arquivo json

--- NodeJS_9_Praticando/praticando_arquivo_json_estado_cidade/index.js
+++ NodeJS_9_Praticando/praticando_arquivo_json_estado_cidade/index.js
@@ -1,5 +1,5 @@
 import { promises as fs } from "fs";
-
+import { Console } from "console";
 
 init();
 var cidades = [];
@@ -30,8 +30,13 @@ async function init() {
             estados.push(estado.Sigla);
         });
 
-        quantidadeCidade('MG');
-        estadoComMaisCidade();
+        //quantidadeCidade('MG');
+        //estadoComMaisCidade();s
+        //estadoComMenosCidade();
+        //cidadeMaiorNomePorEstado();
+        //cidadeMenorNomePorEstado();
+        //cidadeMaiorNome();
+        cidadeMenorNome();
 
     } catch (error) {
 
@@ -59,28 +64,96 @@ const quantidadeCidade = async (uf) => {
 
 async function estadoComMaisCidade() {
     const arr = [];
+    let total = 0;
     for (const estado of estados) {
-        const data = JSON.parse(await fs.readFile('./json/' + estado.toUpperCase() + '.json'));
-        arr.push({ 'uf': estado.uf.toUpperCase(), 'total': data.total });
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        arr.push({ 'uf': estado, 'total': data.total });
     }
     arr.sort((a, b) => {
-        return (a.total > b.total) ? 1 : ((b.total > a.total) ? -1 : 0);
+        return (a.total < b.total) ? 1 : ((b.total < a.total) ? -1 : 0);
     });
     for (let i = 0; i < 5; i++) {
-        console.log(arr[i].uf + ' - ' + await quantidadeCidade(arr[i].uf));
+        console.log(arr[i].uf + ' - ' + arr[i].total);
+        total += arr[i].total;
     }
+    console.log('total: ' + total);
 }
 
 async function estadoComMenosCidade() {
     const arr = [];
+    let total = 0;
     for (const estado of estados) {
-        const data = JSON.parse(await fs.readFile('./json/' + estado.uf.toUpperCase() + '.json'));
-        arr.push({ 'uf': estado.uf.toUpperCase(), 'total': data.total });
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        arr.push({ 'uf': estado, 'total': data.total });
     }
     arr.sort((a, b) => {
         return (a.total > b.total) ? 1 : ((b.total > a.total) ? -1 : 0);
     });
     for (let i = 0; i < 5; i++) {
-        console.log(arr[i].uf + ' - ' + await quantidadeCidade(arr[i].uf));
+        console.log(arr[i].uf + ' - ' + arr[i].total);
+        total += arr[i].total;
     }
+    console.log('total: ' + total);
+}
+
+async function cidadeMaiorNomePorEstado() {
+    let MaiorNome = '';
+    for (const estado of estados) {
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        data.nomes.forEach(dado => {
+            if (dado.cidade.length > MaiorNome.length) {
+                MaiorNome = dado.cidade;
+            }
+        });
+        console.log(`${MaiorNome} - ${estado}`);
+        MaiorNome = '';
+    }
+}
+
+async function cidadeMenorNomePorEstado() {
+    let MenorNome = '';
+    for (const estado of estados) {
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        MenorNome = data.nomes[0].cidade;
+        data.nomes.forEach(dado => {
+            if (dado.cidade.length < MenorNome.length) {
+                MenorNome = dado.cidade;
+            }
+        });
+        console.log(`${MenorNome} - ${estado}`);
+    }
+}
+
+async function cidadeMaiorNome() {
+    let maiorNome = '';
+    let estadoMaiorNome = '';
+    for (const estado of estados) {
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        data.nomes.forEach(dado => {
+            if (dado.cidade.length > maiorNome.length) {
+                maiorNome = dado.cidade;
+                estadoMaiorNome = estado;
+            }
+        });
+    }
+    console.log(`cidade maior nome país: ${estadoMaiorNome} - ${maiorNome}`);
+}
+
+async function cidadeMenorNome() {
+    let menorNome = '';
+    let estadoMenorNome = '';
+    for (const estado of estados) {
+        const data = JSON.parse(await fs.readFile('./json/' + estado + '.json'));
+        if (menorNome == '') {
+            menorNome = data.nomes[0].cidade;
+            estadoMenorNome = estado;
+        }
+        data.nomes.forEach(dado => {
+            if (dado.cidade.length < menorNome.length) {
+                menorNome = dado.cidade;
+                estadoMenorNome = estado;
+            }
+        });
+    }
+    console.log(`cidade menor nome país: ${estadoMenorNome} - ${menorNome}`);
 }
